d591b8fa728bfaa6630fa7bd14fdb569270010f4
Johannes Roeder <johannes.roeder@uni-bremen.de>
2022-10-07 18:23:07 +0200

    Add emission factor constraint

--- modules/oemof_model.py
+++ modules/oemof_model.py
@@ -22,7 +22,8 @@ def create_solph_model(
         capacity_hp_ground=500,
         capacity_electrlysis_el=250,
         capacity_pv=1500,
-        capacity_thermal_storage_m3=1000
+        capacity_thermal_storage_m3=1000,
+        weight_cost_emission=0,
 ):
 
     # initiate the logger (see the API docs for more information)
@@ -48,18 +49,28 @@ def create_solph_model(
     energysystem.add(b_gas, b_elec, b_heat_generation, b_heat_storage_out,
                      b_heat_grid, b_h2)
 
+    var_costs_gas = \
+        weight_cost_emission * techparam["gas_source"]["emission_factor"] + \
+        (1 - weight_cost_emission) * techparam["gas_source"]["variable_costs"]
+
     gas_source = solph.Source(
         label="gas_grid",
         outputs={b_gas: solph.Flow(
-            variable_costs=techparam["gas_source"]["variable_costs"],
+            variable_costs=var_costs_gas,
             emission_factor=techparam["gas_source"]["emission_factor"],
         )}
     )
 
+    var_costs_elec = \
+        weight_cost_emission * techparam["electricity_source"][
+            "emission_factor"] + \
+        (1 - weight_cost_emission) * techparam["electricity_source"][
+            "variable_costs"]
+
     elec_source = solph.Source(
         label="electricity_grid",
         outputs={b_elec: solph.Flow(
-            variable_costs=techparam["electricity_source"]["variable_costs"],
+            variable_costs=var_costs_elec,
             emission_factor=techparam["electricity_source"]["emission_factor"],
         )}
     )
@@ -80,18 +91,29 @@ def create_solph_model(
         )}
     )
 
+    var_costs_elec_sell = \
+        weight_cost_emission * techparam["electricity_sell"][
+            "emission_factor"] + \
+        (1 - weight_cost_emission) * techparam["electricity_sell"][
+            "variable_costs"]
+
     elec_sell = solph.Sink(
         label="elec_sell",
         inputs={b_elec: solph.Flow(
-            variable_costs=techparam["electricity_sell"]["variable_costs"],
+            variable_costs=var_costs_elec_sell,
             emission_factor=techparam["electricity_sell"]["emission_factor"],
         )}
     )
 
+    var_costs_h2_sell = \
+        weight_cost_emission * techparam["hydrogen_sell"]["variable_costs"] + \
+        (1 - weight_cost_emission) * techparam["hydrogen_sell"][
+            "variable_costs"]
+
     h2_sell = solph.Sink(
         label="h2_sell",
         inputs={b_h2: solph.Flow(
-            variable_costs=techparam["hydrogen_sell"]["variable_costs"],
+            variable_costs=var_costs_h2_sell,
             emission_factor=techparam["hydrogen_sell"]["emission_factor"],
         )}
     )
@@ -215,15 +237,14 @@ def create_solph_model(
 
     # plot_es_graph(energysystem, show=True)
 
-    gr = ESGraphRenderer(energy_system=energysystem, filepath="energy_system",
-                         img_format="png")
-    gr.view()
+    # gr = ESGraphRenderer(energy_system=energysystem, filepath="energy_system",
+    #                      img_format="png")
+    # gr.view()
 
     return energysystem
 
 
-def solve_model(energysystem):
-
+def solve_model(energysystem, emission_limit=1000000000):
     solver = "gurobi"  # 'glpk', 'gurobi',....
     debug = False  # Set number_of_timesteps to 3 to get a readable lp-file.
     solver_verbose = True  # show/hide solver output
@@ -232,6 +253,10 @@ def solve_model(energysystem):
 
     # initialise the operational model
     model = solph.Model(energysystem)
+
+    solph.constraints.generic_integral_limit(
+        model, keyword='emission_factor', limit=emission_limit
+    )
 
     # This is for debugging only. It is not(!) necessary to solve the problem and
     # should be set to False to save time and disc space in normal use. For
@@ -246,7 +271,16 @@ def solve_model(energysystem):
 
     # if tee_switch is true solver messages will be displayed
     logging.info("Solve the optimization problem")
-    model.solve(solver=solver, solve_kwargs={"tee": solver_verbose})
+
+    solver_cmdline_options = {
+        # 'threads': 1,
+        # gurobi
+        'MIPGap': 0.001,
+    }
+
+    model.solve(solver=solver, solve_kwargs={"tee": solver_verbose},
+                cmdline_options=solver_cmdline_options
+                )
 
     logging.info("Store the energy system with the results.")
 
@@ -257,7 +291,44 @@ def solve_model(energysystem):
     energysystem.results["main"] = solph.processing.results(model)
     energysystem.results["meta"] = solph.processing.meta_results(model)
 
+    energysystem.results["meta"]["emission_value"] = \
+        model.integral_limit_emission_factor()
+
     return energysystem
+
+
+# print and plot some results
+def plot_results(esys):
+    """
+
+    Args:
+        esys:
+
+    Returns:
+
+    """
+
+    results = esys.results["main"]
+
+    heat_gen = solph.views.node(results, "heat_generation")
+    heat_store = solph.views.node(results, "thermal_storage")
+    elec = solph.views.node(results, "electricity")
+
+    print(heat_gen["sequences"].sum())
+    print(heat_store["sequences"].sum())
+    print(elec["sequences"].sum())
+
+    fig1, ax = plt.subplots(figsize=(10, 5))
+    heat_gen["sequences"].plot(ax=ax)
+    plt.show()
+
+    fig2, ax = plt.subplots(figsize=(10, 5))
+    heat_store["sequences"].plot(ax=ax)
+    plt.show()
+
+    fig3, ax = plt.subplots(figsize=(10, 5))
+    elec["sequences"].plot(ax=ax)
+    plt.show()
 
 
 def calculate_oemof_model(
@@ -325,6 +396,112 @@ if __name__ == '__main__':
 
     fig3, ax = plt.subplots(figsize=(10, 5))
     elec["sequences"].plot(ax=ax)
+    plt.show()
+
+    # ################################
+
+    # what is the minimum possible emission value?
+    esys_emission = create_solph_model(
+        techparam=tech_param,
+        timeindex=time_slice.index,
+        timeseries=time_slice,
+        weight_cost_emission=1,
+    )
+    esys_emission = solve_model(esys_emission)
+    emission_min = esys_emission.results["meta"]["objective"]  # emission in [kg]
+
+    # what is the emission value in the cost optimal case?
+    esys_cost = create_solph_model(
+        techparam=tech_param,
+        timeindex=time_slice.index,
+        timeseries=time_slice,
+        weight_cost_emission=0,
+    )
+
+    esys_max = solve_model(esys_cost)
+    emission_max = esys_max.results["meta"]["emission_value"]
+    cost_max = esys_max.results["meta"]["objective"]
+
+    esys_min = solve_model(esys_cost, emission_limit=emission_min + 0.1)
+    costs_min = esys_min.results["meta"]["objective"]
+
+    factor_emission_reduction = 0.5
+    emission_limit_mid = \
+        factor_emission_reduction * (emission_max - emission_min) + emission_min
+    esys_mid = solve_model(esys_cost, emission_limit=emission_limit_mid)
+    emission_mid = esys_mid.results["meta"]["emission_value"]
+    costs_mid = esys_mid.results["meta"]["objective"]
+
+    # plots
+    fig, ax = plt.subplots()
+    ax.scatter(emission_max, cost_max, color='r')
+    ax.scatter(emission_min, costs_min, color='b')
+    ax.scatter(emission_mid, costs_mid, color='tab:orange')
+    ax.set_xlabel('Emission [kg]')
+    ax.set_ylabel('Costs [€]')
+    ax.grid()
+    # ax.set_title('scatter plot')
+    plt.show()
+
+    for es in [esys_min, esys_max, esys_mid]:
+        plot_results(es)
+        results = es.results["main"]
+        b_elec = solph.views.node(results, "electricity")[
+            "sequences"].sum()
+        print(" ")
+        print(b_elec)
+
+
+    # #####
+
+    heat = []
+    elec = []
+    gas = []
+    h2 = []
+    for es in [esys_min, esys_mid, esys_max]:
+        results = es.results["main"]
+
+        heat_gen = solph.views.node(results, "heat_generation")[
+            "sequences"].sum()
+        heat.append(heat_gen)
+
+        elec_sum = solph.views.node(results, "electricity")[
+            "sequences"].sum()
+        elec.append(elec_sum)
+
+        h2_sum = solph.views.node(results, "h2")[
+            "sequences"].sum()
+        h2.append(h2_sum)
+
+        gas_sum = solph.views.node(results, "gas")[
+            "sequences"].sum()
+        gas.append(gas_sum)
+
+        # b_gas = solph.Bus(label="gas")
+        # b_elec = solph.Bus(label="electricity")
+        # b_heat_generation = solph.Bus(label="heat_generation")
+        # b_heat_storage_out = solph.Bus(label="storage_out")
+        # b_heat_grid = solph.Bus(label="heat_grid")
+        # b_h2 = solph.Bus(label="h2")
+
+    df_heat_all = pd.concat(heat, axis=1)
+    df_heat_all.columns = ["CO2_min", "CO2_mid", "CO2_max"]
+    # df_heat_all = df_heat_all.T
+
+    df_elec = pd.concat(elec, axis=1)
+    df_elec.columns = ["CO2_min", "CO2_mid", "CO2_max"]
+    # df_elec = df_elec.T
+
+    df_gas = pd.concat(gas, axis=1)
+    df_gas.columns = ["CO2_min", "CO2_mid", "CO2_max"]
+    # df_gas = df_gas.T
+
+    df_h2 = pd.concat(h2, axis=1)
+    df_h2.columns = ["CO2_min", "CO2_mid", "CO2_max"]
+    # df_h2 = df_h2.T
+
+    fig, ax = plt.subplots()
+    df_heat_all.plot.bar(stacked=True)
     plt.show()
 
     logging.info("Done!")
