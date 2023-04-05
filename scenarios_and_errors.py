247c7d6010e2fa0c978ca06a3d6e61de6e691a0e
coballee <cristinaoballe@gmail.com>
2023-04-05 13:45:51 +0200

    Added function to generate all error files from the info on the "Error Scenarios.xlsx" file in scenarios_and_errors.py. Generated said error files and changed the "no-error" file's name to "reference".

--- scenarios_and_errors.py
+++ scenarios_and_errors.py
@@ -1,9 +1,20 @@
+import numpy as np
 import pandas as pd
 import os
+import datetime
 
 
 def read_scenarios_names(input_file="Parameter_Values.csv"):
+    """
+    Reads the scenarios names from the Parameter_Values.csv input file and gives them back as a list.
 
+    Arguments:
+        input_file: string. Name of the csv file with the scenarios names in the columns. Default: Parameter_Values.csv
+
+    Returns:
+        scenarios: list of strings. Scenarios names.
+
+    """
     input_path = os.path.join("input", "common", "dimension_scenarios", input_file)
 
     df = pd.read_csv(input_path, delimiter=";")
@@ -13,3 +24,40 @@ def read_scenarios_names(input_file="Parameter_Values.csv"):
         scenarios.append(scenario)
 
     return scenarios
+
+def generating_error_files(input_file="Error Scenarios.xlsx"):
+    """
+    Takes an excel file as reference and generates the different error files from the information contained on
+    the table: the name of the file, the element of the system that is not working and the time interval in which
+    said element is out.
+
+    Args:
+        input_file: string. Name of the excel file with all the error information. Default: Error Scenarios.xlsx.
+
+    """
+
+    path = os.path.join("input", "modelica", "error_scenarios")
+
+    df = pd.read_excel(os.path.join(path, input_file), engine="openpyxl")
+
+    new_columns = df.columns[8:]
+    for index in df.index[1:]:
+        filename = df.iloc[index]["error type"] + ".csv"
+        new_file_index = np.linspace(0, 31532400, 8760, dtype=int)
+        new_df = pd.DataFrame(data=1, index=new_file_index, columns=new_columns)
+        new_df["date"] = pd.date_range("2018-01-01", periods=new_df.shape[0], freq="H").strftime("%d.%m.%Y %H:%M")
+
+        start_time = df.iloc[index].start
+        end_time = df.iloc[index].end
+
+        num = int((end_time - start_time) / 3600)
+
+        filtered_indices_list = list(np.linspace(start_time, end_time, num, endpoint=False, dtype=int))
+
+
+        for filtered_index in filtered_indices_list:
+            new_df.loc[new_df.index == filtered_index, new_columns] = df.iloc[index][new_columns].array
+
+        new_df.to_csv(os.path.join(path, filename))
+
+
