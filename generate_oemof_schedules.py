ea92e3aad28ab3d2928d1e6361a6ffda9acd3581
Johannes Roeder <johannes.roeder@uni-bremen.de>
2023-04-25 23:10:13 +0200

    Add global scenario input

--- generate_oemof_schedules.py
+++ generate_oemof_schedules.py
@@ -3,37 +3,55 @@ import modules.oemof_model as om
 
 # Input
 
-dimension_scenario = "Scenario-A"
-
-simulation_period = ("01-01-2022", 2)
-factor_emission_reduction = 0.5 #between 0 (cost-optimized) and 1 (emission-optimized)
-
 path_oemof = os.path.join("input", "solph")
 
-scenario_name = "_".join(
-        [dimension_scenario, "ER-" + str(factor_emission_reduction),
-         simulation_period[0] + "-" + str(simulation_period[1])]
-    )
 
-# Calculation
+simulation_period = ("01-01-2018", 7)
+factor_emission_reduction = 0.5 #between 0 (cost-optimized) and 1 (emission-optimized)
 
-schedules = om.calculate_oemof_model(
-    dimension_scenario=dimension_scenario,
-    simulation_period=simulation_period,
-    factor_emission_reduction=factor_emission_reduction,
-    path_oemof=path_oemof,
-)
 
-# export oemof results
-schedules.to_csv(os.path.join("results", "oemof_raw", scenario_name + ".csv"))
+global_scenarios = [
+    # "2020",
+    # "2030-syn-gas-low",
+    "2030-syn-gas-high",
+    # "2050-syn-gas-low",
+    # "2050-syn-gas-high",
+]
 
-# create schedules for modelica (function needs to be done)
-# TODO : complete function
-schedules_for_modelica = om.prepare_schedules(schedules)
+dimension_scenarios = [
+    "Scenario-A",
+    # "Scenario-B",
+    # "Scenario-C",
+]
 
-# export schedules
-schedules_for_modelica.to_csv(
-    os.path.join("results", "modelica_schedules", scenario_name + ".csv")
-)
+for global_sc in global_scenarios:
+    for dim_sc in dimension_scenarios:
+
+        scenario_name = "_".join(
+                [global_sc, dim_sc, "ER-" + str(factor_emission_reduction),
+                 simulation_period[0] + "_" + str(simulation_period[1])]
+            )  # ER: emission reduction
+
+        # Calculation
+
+        schedules = om.calculate_oemof_model(
+            dimension_scenario=dim_sc,
+            simulation_period=simulation_period,
+            factor_emission_reduction=factor_emission_reduction,
+            path_oemof=path_oemof,
+            global_scenario=global_sc,
+        )
+
+        # export oemof results
+        schedules.to_csv(os.path.join("results", "oemof_raw", scenario_name + ".csv"))
+
+        # create schedules for modelica (function needs to be done)
+        # TODO : complete function
+        schedules_for_modelica = om.prepare_schedules(schedules)
+
+        # export schedules
+        schedules_for_modelica.to_csv(
+            os.path.join("results", "modelica_schedules", scenario_name + ".csv")
+        )
 
 print("finished.")
